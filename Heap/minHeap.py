4e9294faeb05f6523eee68b070e874f2e987065f
Mohn Kummr <54698755+mohannn-sys@users.noreply.github.com>
2024-01-17 16:37:48 +0000

    Create minHeap.py

---
+++ Heap/minHeap.py
@@ -0,0 +1,62 @@
+class MinHeap:
+    def __init__(self):
+        self.heap = []
+
+    def insert(self, value):
+        self.heap.append(value)
+        self._heapify_up()
+
+    def extract_min(self):
+        if not self.heap:
+            return None  # Heap is empty
+        if len(self.heap) == 1:
+            return self.heap.pop()
+
+        min_value = self.heap[0]
+        self.heap[0] = self.heap.pop()
+        self._heapify_down()
+        return min_value
+
+    def _heapify_up(self):
+        index = len(self.heap) - 1
+        while index > 0:
+            parent_index = (index - 1) // 2
+            if self.heap[index] < self.heap[parent_index]:
+                self.heap[index], self.heap[parent_index] = self.heap[parent_index], self.heap[index]
+                index = parent_index
+            else:
+                break
+
+    def _heapify_down(self):
+        index = 0
+        while True:
+            left_child_index = 2 * index + 1
+            right_child_index = 2 * index + 2
+            smallest = index
+
+            if left_child_index < len(self.heap) and self.heap[left_child_index] < self.heap[smallest]:
+                smallest = left_child_index
+
+            if right_child_index < len(self.heap) and self.heap[right_child_index] < self.heap[smallest]:
+                smallest = right_child_index
+
+            if smallest != index:
+                self.heap[index], self.heap[smallest] = self.heap[smallest], self.heap[index]
+                index = smallest
+            else:
+                break
+
+# Example usage:
+min_heap = MinHeap()
+min_heap.insert(10)
+min_heap.insert(20)
+min_heap.insert(15)
+min_heap.insert(40)
+min_heap.insert(50)
+min_heap.insert(25)
+
+print("Min Heap:", min_heap.heap)
+
+min_value = min_heap.extract_min()
+print("Extracted min value:", min_value)
+print("Min Heap after extraction:", min_heap.heap)
